89685ced41228d9bee02ad613948ebb106f38e94
holteli000 <holteli000@warren.k12.in.us>
2021-12-14 12:46:23 -0500

    precaution submit

--- src/EscapeRoom.java
+++ src/EscapeRoom.java
@@ -1,5 +1,102 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 public class EscapeRoom {
     public static void main(String[] args) {
 
+
+        JFrame window = new JFrame("Escape room");
+        JPanel panel = new JPanel();
+        JButton door = new JButton("Door");
+        JButton dresser = new JButton("dresser");
+
+
+        window.setSize(350, 100);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        //add button listeners
+        door.addActionListener(new door());
+
+        dresser.addActionListener(new door.dresser());
+
+
+
+
+        panel.add(door);
+        panel.add(dresser);
+
+
+        window.add(panel);
+        window.setVisible(true);
+
+
+
     }
-}
+
+private static class door implements ActionListener {
+
+    public void actionPerformed(ActionEvent actionEvent) {
+
+
+        JFrame window = new JFrame("door");
+        JPanel panel = new JPanel();
+        JButton keyhole = new JButton("keyhole");
+
+        window.setSize(350, 100);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        panel.add(keyhole);
+
+        window.add(panel);
+        window.setVisible(true);
+
+
+    }
+
+    public static class dresser implements ActionListener {
+
+        public void actionPerformed(ActionEvent actionEvent) {
+            JFrame window = new JFrame("dresser");
+            JPanel panel = new JPanel();
+            JButton key = new JButton("key");
+
+
+            window.setSize(350, 100);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.setLocationRelativeTo(null);
+
+
+            panel.add(key);
+
+            window.add(panel);
+            window.setVisible(true);
+
+
+           }
+        }
+
+
+
+
+
+
+
+
+      }
+   }
+
+
+
+
+
+
+
+/*  1. figure out escape room idea
+    2. put together simple gui for now
+    3. associate buttons with escape room
+    4. use if statements for a condition
+ */
